f746dc1c4f505d7417d61c2dc63e98785885368b
Daniel Campos Olivares <dacamposol@gmail.com>
2018-01-29 18:46:44 +0100

    Some methods and explanations added.

--- OKMObjects/okmDocument.py
+++ OKMObjects/okmDocument.py
@@ -8,6 +8,19 @@ class OkmDocument(object):
         self.auth = HTTPBasicAuth(username, password)
         self.url = url.__add__('/services/rest/document')
 
+# We are able to upload a document to our system taking the path where it is locally.
+    # The information returned includes the id of the document in the manager.
+
+    def uploadDocument(self, srcPath):
+        request = self.url.__add__('/create')
+        multipart_form_data = {
+            'file': open(srcPath, 'rb')
+        }
+        r = requests.post(request, files=multipart_form_data,
+                          headers={'Content-Type': 'application/json'})
+
+# We obtain the document selected in the source path and we save it with the destination name.
+
     def getDocument(self, srcPath, dstName):
         payload = {'docId': srcPath}
         request = self.url.__add__('/getContent')
@@ -15,6 +28,8 @@ class OkmDocument(object):
         with open(''.__add__(dstName), 'wb') as file:
             file.write(r.content)
         assert r.status_code == 200
+
+# We get a JSON with the version history of a concrete document, the JSON is saved in a file.
 
     def getDocumentVersionHistory(self, srcPath):
         payload = {'docId': srcPath}
@@ -26,6 +41,8 @@ class OkmDocument(object):
             file.write(r.text)
         assert r.status_code == 200
 
+# We get the properties of a document and we save them into a JSON.
+
     def getDocumentProperties(self, srcPath):
         payload = {'docId': srcPath}
         request = self.url.__add__('/getProperties')
@@ -36,6 +53,8 @@ class OkmDocument(object):
             file.write(r.text)
         assert r.status_code == 200
 
+# We copy a document to another new path.
+
     def copyDocument(self, srcPath, dstPath):
         payload = {'docId': srcPath, 'dstId': dstPath}
         request = self.url.__add__('/copy')
@@ -43,8 +62,16 @@ class OkmDocument(object):
                          auth=self.auth)
         assert r.status_code == 204
 
+# We delete a document of a concrete path.
+
     def deleteDocument(self, srcPath):
         payload = {'docId': srcPath}
         request = self.url.__add__('/delete')
         r = requests.delete(request, params=payload, auth=self.auth)
         assert r.status_code == 204
+
+# We move a document from a sorcePath to another one.
+
+    def moveDocument(self, srcPath, dstPath):
+        self.copyDocument(srcPath, dstPath)
+        self.deleteDocument(srcPath)
